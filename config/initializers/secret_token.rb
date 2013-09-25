be504bf521806673319d1532a17c89df955b63e4
Franz Pereira <frnz.cr@gmail.com>
2013-09-25 00:01:46 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EnsolabsStatic::Application.config.secret_key_base = '849b3449167ff586a55019f3496894a6f6989a8bd44ac01857c750ce8ed6fa29280ce15fd96ceba760139a9e4ba674b9f4dc710d2c5e363f7f53cdbfcd8dc616'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EnsolabsStatic::Application.config.session_store :cookie_store, key: '_ensolabs-static_session'
